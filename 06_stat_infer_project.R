1f13aadb5ff25cd243d5d159d1842401c6c37953
manos-atx <emm.kalaitzakis@gmail.com>
2021-06-29 09:16:22 +0300

    06 stat infer update

---
+++ 06_stat_infer_project.R
@@ -0,0 +1,32 @@
+
+n <- 40
+lambda <- 0.2
+mu <- 1/lambda
+sigma <- 1/lambda
+
+iter <- 1000
+
+set.seed(1)
+mc <- matrix(rexp(iter*n, lambda), nrow = iter, ncol = n)
+
+mus <- apply(mc, 1, mean)
+vars <- apply(mc, 1, var)
+
+library(ggplot2)
+
+ggplot(data.frame(means = mus), aes(means)) +
+  geom_histogram()
+
+ggplot(data.frame(variances = vars), aes(variances)) +
+  geom_histogram()
+
+library(datasets)
+
+str(ToothGrowth)
+summary(ToothGrowth)
+
+tapply(ToothGrowth$len, ToothGrowth$supp, mean)
+tapply(ToothGrowth$len, ToothGrowth$dose, mean)
+tapply(ToothGrowth$len, list(ToothGrowth$supp, ToothGrowth$dose), mean)
+
+
